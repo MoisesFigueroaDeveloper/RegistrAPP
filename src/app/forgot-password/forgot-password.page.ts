568569cb610488a8ecda71c614f08350308f375c
MoisesFigueroaDeveloper <moisesfigueroadeveloper@gmail.com>
2023-08-31 16:35:16 -0400

    0.0.2 forgot-password

---
+++ src/app/forgot-password/forgot-password.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.page.html',
+  styleUrls: ['./forgot-password.page.scss'],
+})
+export class ForgotPasswordPage implements OnInit {
+  email: string = '';
+
+  constructor(private router: Router) {}
+
+  sendPasswordResetEmail() {
+    // Aquí implementa la lógica para enviar un correo de recuperación de contraseña
+    // Puedes utilizar servicios, autenticación, etc.
+
+    // Después de enviar el correo, puedes redirigir al usuario a una página de confirmación
+    this.router.navigate(['/login']);
+  }
+
+  ngOnInit() {}
+}
+
