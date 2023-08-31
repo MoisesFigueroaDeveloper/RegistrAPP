482c28332271a4fdd8f01dda2c2dbcaa9e354892
MoisesFigueroaDeveloper <moisesfigueroadeveloper@gmail.com>
2023-08-31 16:27:40 -0400

    0.0.1 register

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  username: string = '';
+  email: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  register() {
+    // Aquí implementa la lógica para registrar al usuario en tu sistema
+    // Puedes utilizar servicios, llamadas a la base de datos, etc.
+
+    // Después de registrar al usuario, puedes redirigirlo a la página de inicio de sesión
+    this.router.navigate(['/login']);
   }
 
+  ngOnInit() {}
 }
